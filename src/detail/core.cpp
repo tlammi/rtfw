e96d2e3deffa3c7271d0bef3cd2befea2d933da6
tlammi <tangojulietlima@gmail.com>
2021-03-20 14:16:27 +0200

    Ensure correct destruction order

--- src/detail/core.cpp
+++ src/detail/core.cpp
@@ -1,27 +1,49 @@
 #include "rtfw/detail/core.hpp"
 
 
+#include <iostream>
+
+
 
 namespace rtfw{
 namespace detail{
-
 using namespace std::literals::chrono_literals;
+
+
+namespace {
+
+void init(List& init_stack, List& module_list){
+	ListHead* iterator = module_list.first();
+	while(iterator){
+		auto* target = iterator;
+		iterator = List::next(*iterator);
+		init_stack.push_back(*target);
+		std::cerr << "initializing " << static_cast<ModuleHolder*>(target)->name() << '\n';
+		static_cast<ModuleHolder*>(target)->init();
+		module_list.push_front(*target);
+	}
+}
+
+
+void deinit(List& module_list){
+	ListHead* iterator = module_list.first();
+	while(iterator){
+		auto* target = static_cast<ModuleHolder*>(iterator);
+		std::cerr << "destroying " << target->name() << '\n';
+		iterator = List::next(*iterator);
+		target->clear();
+	}
+
+}
+}
 
 Core::Core(){}
 
 Core::~Core(){}
 void Core::run(){
-	ListHead* iterator = ModuleHolder::instances.first();
-	while(iterator){
-		auto* target = iterator;
-		iterator = List::next(*iterator);
-		init_stack_.push_back(*target);
-		static_cast<ModuleHolder*>(target)->init();
-		ModuleHolder::instances.push_front(*target);
-	}
-
-	std::this_thread::sleep_for(10s);
-
+	init(init_stack_, ModuleHolder::instances);
+	std::this_thread::sleep_for(1s);
+	deinit(ModuleHolder::instances);
 }
 
 Core core{};
